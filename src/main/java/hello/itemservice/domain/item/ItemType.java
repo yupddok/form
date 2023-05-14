c419e0aef60a94b8518238935cf76abc8cf0aafb
yupddok <yupddok@gmail.com>
2023-05-14 22:55:26 +0900

    타임리프 - 스프링 통합과 폼 > 요구사항 추가, 체크 박스 - 단일1

---
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,14 @@
+package hello.itemservice.domain.item;
+
+/**
+ * 상품 종류
+ */
+public enum ItemType {
+    BOOK("도서"), FOOD("음식"), ETC("기타");
+
+    private final String description;
+
+    ItemType(String description) {
+        this.description = description;
+    }
+}
